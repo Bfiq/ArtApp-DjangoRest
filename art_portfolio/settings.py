c5c5722988fb2d462e3cc380e915419af380e7b6
Brian Fiquitiva <brianfiquitiva12@gmail.com>
2024-06-08 13:29:16 -0500

    endpoint para registro de usuarios y creación de token

--- art_portfolio/settings.py
+++ art_portfolio/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'api'
 ]
 
@@ -100,6 +102,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
 
 
 # Internationalization
